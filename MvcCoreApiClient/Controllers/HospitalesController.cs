da9d0164da08827b42843997260c96522a2feaf9
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-03-25 10:11:18 +0100

    Api con service

---
+++ MvcCoreApiClient/Controllers/HospitalesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiClient.Models;
+using MvcCoreApiClient.Services;
+
+namespace MvcCoreApiClient.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        private ServiceHospitales service;
+
+        public HospitalesController(ServiceHospitales service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Servidor()
+        {
+            List<Hospital> hospitales =
+                await this.service.GetHospitalesAsync();
+            return View(hospitales);
+        }
+
+    }
+}
---
+++ MvcCoreApiClient/Services/ServiceHospitales.cs
@@ -0,0 +1,52 @@
+﻿using MvcCoreApiClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MvcCoreApiClient.Services
+{
+    public class ServiceHospitales
+    {
+        private string apiUrl;
+        //NECESITAMOS INDICAR EL TIPO DE DATOS QUE VAMOS A LEER
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceHospitales()
+        {
+            this.apiUrl = "https://apicorehospitalesjra-fmcaf9d2haacbph2.italynorth-01.azurewebsites.net/";
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Hospital>> GetHospitalesAsync()
+        {
+            //SE UTILIZA LA CLASE HTTPCLIENT PARA LAS PETICIONES
+            using(HttpClient client = new HttpClient())
+            {
+                string request = "api/hospital";
+                //INDICAMOS EL HOST
+                client.BaseAddress = new Uri(this.apiUrl);
+                //INDICAMOS LOS DATOS QUE VAMOS A CONSUMIR
+                //LIMPIAMOS LAS CABECERAS POR NORMA
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                //REALIZAMOS LA PETICION Y CAPTURAMOS UNA RESPUESTA
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                //EN LA RESPUESTA TENEMOS LA CLAVE SI DESEAMOS PERSONALIZAR ERRORES
+                if (response.IsSuccessStatusCode)
+                {
+                    //RECUPERAMOS CONTENIDO EN JSON
+                    string json =
+                        await response.Content.ReadAsStringAsync();
+                    //MEDIANTE NEWTON DESERIALIZAMOS JSON A LIST
+                    List<Hospital> hospitales = JsonConvert.DeserializeObject<List<Hospital>>(json);
+                    return hospitales;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
